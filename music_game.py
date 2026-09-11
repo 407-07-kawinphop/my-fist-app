13e515dc9f60e46a7e472d0f0287389fa64128bf
407-07-kawinphop <54179@yupparaj.ac.th>
2026-09-11 23:12:36 +0700

    Update music_game.py

--- music_game.py
+++ music_game.py
@@ -63,7 +63,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5):
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
 
-  if u_ans5 == "ฟ้า-Tattoo colour":
+     if u_ans5 == "ฟ้า-Tattoo colour":
         st.success("✅ ข้อ 5: ถูกต้อง")
         score += 1
     else:
